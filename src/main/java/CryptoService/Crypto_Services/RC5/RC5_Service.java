10edca24a91fa94584d560c16e46b8e07d5e3a52
g.yakovlev <g.yakovlev@NPO5256>
2023-11-20 12:47:51 +0300

    аписал перемешивание rc5

--- src/main/java/CryptoService/Crypto_Services/RC5/RC5_Service.java
+++ src/main/java/CryptoService/Crypto_Services/RC5/RC5_Service.java
@@ -33,22 +33,67 @@ public class RC5_Service {
     public static String Get_Cipher_Text() {
         Split_Key_to_Words();
         Get_Wide_Keys();
+        Mixing();
         return "ffff";
     }
 
-    private static void Mixing(){
-        int N = 3 * c > 3 * 2 * (R + 1) ? 3 * c : 3 * 2 * (R + 1);
-        BigInteger G = new BigInteger("0"); BigInteger H = new BigInteger("0"); int i = 0; int j = 0;
+    private static void Make_Cipher(){
 
-        for(int index = 0; index < N; index++){
-            G = WideKeysTable.get(i) = WideKeysTable.get(i).add(G) + G + H
+    }
+
+    private static void Mixing() {
+        int N = 3 * c > 3 * 2 * (R + 1) ? 3 * c : 3 * 2 * (R + 1);
+        BigInteger G = new BigInteger("0");
+        BigInteger H = new BigInteger("0");
+        int i = 0;
+        int j = 0;
+
+        for (int index = 0; index < N; index++) {
+            G = (WideKeysTable.get(i).add(G)).add(H);
+
+            String G_str = Get_String_View(G.toByteArray());
+            G_str = Left_Shift_String(G_str, 3);
+            G = new BigInteger(G_str, 2);
+
+            H = (RoundKeysWords.get(j).add(G)).add(H);
+
+            int temp = (G.add(H)).mod(new BigInteger(String.valueOf(W))).intValue();
+            int len = Integer.parseInt("" + temp);
+            String H_str = Get_String_View(H.toByteArray());
+            H_str = Left_Shift_String(G_str, len);
+            H = new BigInteger(H_str, 2);
+
+            WideKeysTable.set(i, G);
+            RoundKeysWords.set(j, H);
+
+            i = (i + 1) % 2 * (R + 1);
+            j = (j + 1) % c;
         }
+    }
+
+    public static String Left_Shift_String(String byteRow, int shift) {
+        for (int i = 0; i < shift; i++) {
+            String value0 = byteRow.substring(0, 1);
+            byteRow = byteRow.substring(1, byteRow.length());
+            byteRow = byteRow + value0;
+        }
+
+        return byteRow;
+    }
+
+    private static String Get_String_View(byte[] arr) {
+        int[] mas = new int[arr.length];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = arr[i];
+        }
+
+        return convertService.Get_Bit_View(mas);
     }
 
     private static void Get_Wide_Keys() {
         int index = 2 * (R + 1) - 1;
         WideKeysTable.add(P_const);
-        for(int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++) {
             BigInteger key = WideKeysTable.get(i).add(Q_const);
             WideKeysTable.add(key);
         }
