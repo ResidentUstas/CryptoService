eb9587d135de64b493b7814a925ceeeaa1cf8191
g.yakovlev <g.yakovlev@NPO5256>
2023-12-01 15:35:50 +0300

    написал  шифрование DES

--- src/main/java/CryptoService/Crypto_Services/DES/Des_Service.java
+++ src/main/java/CryptoService/Crypto_Services/DES/Des_Service.java
@@ -5,29 +5,128 @@ import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
 
 public class Des_Service {
     private String Key = "AABB09182736CCDD";
     private int[] PC1 = Des_Initials.PC1;
+    private int[] PC2 = Des_Initials.PC2;
+    private int[] IPb = Des_Initials.IP_back;
+    private int[] IP = Des_Initials.IP;
+    private int[] PBox_Straight = Des_Initials.pbox_straight;
+    private int[][] sbox1 = Des_Initials.sbox1;
+    private int[][] sbox2 = Des_Initials.sbox2;
+    private int[][] sbox3 = Des_Initials.sbox3;
+    private int[][] sbox4 = Des_Initials.sbox4;
+    private int[][] sbox5 = Des_Initials.sbox5;
+    private int[][] sbox6 = Des_Initials.sbox6;
+    private int[][] sbox7 = Des_Initials.sbox7;
+    private int[][] sbox8 = Des_Initials.sbox8;
+    private int[][][] sbox_collection = new int[][][]{sbox1, sbox2, sbox3, sbox4, sbox5, sbox6, sbox7, sbox8};
+    private int[] P_Extention = Des_Initials.P_Extention;
+    private int[] RoundKeys = new int[16];
+    private String[] RoundKeysStr = new String[16];
     private int[] Key_shifts = Des_Initials.KEY_SHIFTS;
     private final ConvertService convertService = new ConvertService();
 
     public void KeyExtension() {
+        if (RoundKeysStr[0] != null) {
+            return;
+        }
+
         int[] keyBytes = convertService.Get_ByteRow_From_String(Key, 8);
         ArrayUtils.reverse(keyBytes);
         String key_bits = convertService.Get_Bit_View(keyBytes);
         key_bits = Permutation(PC1, key_bits);
         String Ci = key_bits.substring(0, 28);
-        String Di = key_bits.substring(18, key_bits.length());
+        String Di = key_bits.substring(28, key_bits.length());
         for (int i = 0; i < Key_shifts.length; i++) {
-
+            Ci = Left_Shift(Ci, Key_shifts[i]);
+            Di = Left_Shift(Di, Key_shifts[i]);
+            String Round_key = Permutation(PC2, Ci + Di);
+            RoundKeysStr[i] = Round_key;
         }
+    }
+
+    public String Get_Cipher_Text(String Block) {
+        Block = "123456ABCD132536";
+        int[] bytes = convertService.Get_ByteRow_From_String(Block, 8);
+        ArrayUtils.reverse(bytes);
+        Block = convertService.Get_Bit_View(bytes);
+        Block = Permutation(IP, Block);
+        Block = Feistel_net(Block);
+        Block = Permutation(IPb, Block);
+        Long result_L = Long.parseLong(Block.substring(0, 32), 2);
+        Long result_R = Long.parseLong(Block.substring(32, Block.length()), 2);
+        Block = Long.toHexString(result_L) + Long.toHexString(result_R);
+        return Block;
+    }
+
+    private String Feistel_net(String OpenBlock) {
+        String L = OpenBlock.substring(0, 32);
+        String R = OpenBlock.substring(32, OpenBlock.length());
+        String L0 = R;
+        for (int i = 0; i < 16; i++) {
+            R = Des_Function(R, i);
+            R = XOR(R, L);
+            L = L0;
+            L0 = R;
+        }
+
+        return R + L;
+    }
+
+    private String view(String L) {
+        Long x = Long.parseLong(L, 2);
+        String y = Long.toHexString(x);
+        return y;
+    }
+
+    private String Des_Function(String R, int round) {
+        String R_four = "";
+        R = Permutation(P_Extention, R);
+        R = XOR(R, RoundKeysStr[round]);
+        for (int j = 0; j < 8; j++) {
+            String R_six = R.substring(j * 6, j * 6 + 6);
+            R_four += Sbox_display(R_six, sbox_collection[j]);
+        }
+
+        R_four = Permutation(PBox_Straight, R_four);
+
+        return R_four;
+    }
+
+    private String Sbox_display(String R_six, int[][] sbox) {
+        String binary1 = String.valueOf(R_six.charAt(0)) + String.valueOf(R_six.charAt(5));
+        String binary2 = String.valueOf(R_six.charAt(1)) + String.valueOf(R_six.charAt(2))
+                + String.valueOf(R_six.charAt(3)) + String.valueOf(R_six.charAt(4));
+        int i = Integer.parseInt(binary1, 2);
+        int j = Integer.parseInt(binary2, 2);
+
+        String result = Integer.toString(sbox[i][j], 2);
+        while (result.length() < 4) {
+            result = "0" + result;
+        }
+        return result;
+    }
+
+    private String XOR(String a, String b) {
+        String result = "";
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == b.charAt(i)) {
+                result += "0";
+            } else {
+                result += "1";
+            }
+        }
+
+        return result;
     }
 
     private String Permutation(int[] permutation, String val) {
         String result = "";
         for (int i = 0; i < permutation.length; i++) {
-            result += val.charAt(permutation[i]);
+            result += val.charAt(permutation[i] - 1);
         }
 
         return result;
--- src/main/java/CryptoService/Crypto_Services/DES/Des_cipher.java
+++ src/main/java/CryptoService/Crypto_Services/DES/Des_cipher.java
@@ -1,4 +1,40 @@
 package CryptoService.Crypto_Services.DES;
 
+import CryptoService.Services.IOService;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class Des_cipher {
+    Des_Service des_service = new Des_Service();
+    public String Get_Cipher_Text(String OpenText) throws IOException, DecoderException {
+        des_service.KeyExtension();
+        String OpenTextHex = IOService.ReadBytesFromString(OpenText);
+
+        String result = "";
+        while (OpenTextHex.length() > 0) {
+            String openBlock = OpenTextHex.substring(0, 16);
+            OpenTextHex = OpenTextHex.substring(16, OpenTextHex.length());
+
+            //Дополним последний неполный блок
+            if (OpenTextHex.length() < 16 && OpenTextHex.length() > 0) {
+                while (OpenTextHex.length() < 16) {
+                    OpenTextHex += "0";
+                }
+            }
+
+            //Зашифруем полученный блок
+            String cipher_result = des_service.Get_Cipher_Text(openBlock);
+            result += cipher_result;
+        }
+
+        IOService.WriteStringToFile(result, "D:\\CipherBlowFishRes.txt");
+        return result;
+    }
+
+    public String Get_Open_Text(String CipherText) throws IOException, DecoderException {
+      return "dfd";
+    }
 }
