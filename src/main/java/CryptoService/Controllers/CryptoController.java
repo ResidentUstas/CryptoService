10b1643fd0d93d6cb14660394b06f2a4754611e1
g.yakovlev <g.yakovlev@NPO5256>
2023-12-05 18:34:14 +0300

    Доработки проекта

--- src/main/java/CryptoService/Controllers/CryptoController.java
+++ src/main/java/CryptoService/Controllers/CryptoController.java
@@ -1,14 +1,10 @@
 package CryptoService.Controllers;
 
-import CryptoService.Models.CipherModel;
-import CryptoService.Models.OperModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
 @Controller
 @RequestMapping("/crypto")
 public class CryptoController {
